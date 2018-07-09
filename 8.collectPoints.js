3834b88db705918d32345b933fa8ae9c8e059803
jawache <jawache@gmail..com>
2018-07-09 15:37:26 +0300

    Cleaned up the collect points example

--- 8.collectPoints.js
+++ 8.collectPoints.js
@@ -1,12 +1,21 @@
+/**
+ * In this example we start collecting X,Y point from the mouse clicks.
+ * We store them in global variables X,Y as normalised values, instead of (22,333) it will be something like (0.1212, 0.21212) with the max height as 1 and the max width as 1
+ */
+
 // This will store mouse x,y points that have been scaled from 0->1
 X = [];
 Y = [];
 
 // This scales a value from 0 to max to 0 to 1
 const norm = (x, max) => map(x, 0, max, 0, 1);
+const normX = x => norm(x, windowWidth);
+const normY = x => norm(x, windowHeight);
 
 // This scales a value from 0 to 1 to 0 to max
 const denorm = (x, max) => map(x, 0, 1, 0, max);
+const denormX = x => denorm(x, windowWidth);
+const denormY = x => denorm(x, windowHeight);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -17,10 +26,8 @@ This function is called every time a mouse is clicked.
 */
 function mouseClicked() {
   console.log("Clicked", `${mouseX}, ${mouseY}`);
-  // 3
-  // We can print an elipse for each mouse click
-  let Xnorm = norm(mouseX, windowWidth);
-  let Ynorm = norm(mouseY, windowHeight);
+  let Xnorm = normX(mouseX);
+  let Ynorm = normY(mouseY);
   X.push(Xnorm);
   Y.push(Ynorm);
   console.log([X, Y]);
